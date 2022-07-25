927b366bed628e616de78caefe37cea3672e87ae
Arpitpatel1771 <arpitpatel1771@gmail.com>
2022-07-25 18:01:38 +0530

    init2

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,36 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <NavBar />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NavBar from "./components/NavBar.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    name: "App",
+    components: {
+        NavBar,
+    },
+};
 </script>
 
 <style>
+@import url("https://rsms.me/inter/inter.css");
+html {
+    font-family: "Inter", sans-serif;
+}
+@supports (font-variation-settings: normal) {
+    html {
+        font-family: "Inter var", sans-serif;
+    }
+}
+
+body {
+    margin: 0;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: "Inter", Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
 }
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="wrapper">
+        <div class="button">
+            test
+        </div>
+    </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+.wrapper{
+    position: fixed;
+    
+}
+</style>
